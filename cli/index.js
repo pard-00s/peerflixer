f2be2842d4dfaf406c3d167d036bb958b4299b71
Geno Roupsky <geno@roupsky.name>
2018-04-26 15:17:41 +0300

    Option to limit peer connections

--- cli/index.js
+++ cli/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const os = require('os')
 const pkg = require('../package.json')
 const server = require('..')
 
@@ -11,24 +12,24 @@ const argv = require('yargs')
   .usage('$0 <command> [options]')
   .help()
   .version(pkg.version)
-  .option('verbose', {
-    alias: 'v',
-    default: false
+  .option('connections', {
+    alias: 'c',
+    default: os.cpus().length > 1 ? 100 : 30,
+    number: true,
+    describe: 'Max connected peers per torrent'
   })
-  .options('port', {
-    alias: 'p',
-    default: 8080,
-    number: true
-  })
-  .options('hostname', {
+  .option('hostname', {
     alias: 'h',
     default: 'localhost',
-    string: true
+    string: true,
+    describe: 'Host name or IP to bind the server to'
   })
+  .option('port', { alias: 'p', default: 8080, number: true, describe: 'Change the http port' })
+  .option('verbose', { alias: 'v', default: false })
   .argv
 /* eslint-enable no-unused-expressions */
 
 const srv = server(argv)
 srv.on('listening', function () {
-  console.log(`Started listening at http://${argv.hostname||'localhost'}:${srv.address().port}`)
+  console.log(`Started listening at http://${argv.hostname || 'localhost'}:${srv.address().port}`)
 })
